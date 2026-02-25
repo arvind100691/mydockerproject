790f3e1e8bdf6a4efdd38e78d2edbe6327aa0a0f
arvind100691 <arvind100691@gmail.com>
2026-02-25 07:09:37 +0000

    Stop tracking build/ directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/mydockerproject
-# Build directory: /workspaces/mydockerproject/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
-subdirs("tests")
--- build/tests/sample_test[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[Sanity.OnePlusOne]=]  /workspaces/mydockerproject/build/tests/sample_test [==[--gtest_filter=Sanity.OnePlusOne]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[Sanity.OnePlusOne]=]  PROPERTIES WORKING_DIRECTORY /workspaces/mydockerproject/build/tests SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  sample_test_TESTS Sanity.OnePlusOne)
